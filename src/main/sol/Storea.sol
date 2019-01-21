ef75c4374bf21429d92ce04e80e003c9705ed579
amalraj.manigmail.com <amalraj.manigmail.com>
2019-01-21 12:22:47 +0800

    update run test script and solc version

--- src/main/sol/Storea.sol
+++ src/main/sol/Storea.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.5.0;
+pragma solidity ^0.4.21;
 
 interface storeb {
     function setb(uint x) external;
--- src/main/sol/Storeb.sol
+++ src/main/sol/Storeb.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.5.0;
+pragma solidity ^0.4.21;
 
 interface storec {
     function setc(uint x) external;
